6c923bb9ef054eb6bd39bb5794cc61c09286aa2b
obaidaattaee <obaataii@gmail.com>
2020-09-03 22:06:14 +0300

    repair a trendeng section

--- resources/views/site/trending.blade.php
+++ resources/views/site/trending.blade.php
@@ -1,90 +1,62 @@
+@php
+use App\Models\Product ;
+$products = Product::orderBy('reviews')->take(6)->get();
+
+@endphp
 <div class="site-section bg-light">
-      <div class="container">
+    <div class="container">
         <div class="row mb-5">
-          <div class="col-md-7 text-left border-primary">
-            <h2 class="font-weight-light text-primary">Trending Today</h2>
-          </div>
+            <div class="col-md-7 text-left border-primary">
+                <h2 class="font-weight-light text-primary">Trending Today</h2>
+            </div>
         </div>
         <div class="row mt-5">
-          <div class="col-lg-6">
-           @php $trends = \App\Models\Product::get() @endphp
-            @foreach ($trends as $trend )
-            <div class="d-block d-md-flex listing">
-                  <a href="{{ route('listings') }}" class="img d-block" style="background-image: url('{{asset('storage/'.$trend->image)}}"></a>
-                  <div class="lh-content">
-                    <span class="category">{{ $trend->category_id }}</span>
-                    <a href="#" class="bookmark"><span class="icon-heart"></span></a>
-                    <h3><a href="listings-single.html">{{ $trend->title }}</a></h3>
-                    <address>{{ $trend->address }}</address>
-                    <p class="mb-0">
-                      <span class="icon-star text-warning"></span>
-                      <span class="icon-star text-warning"></span>
-                      <span class="icon-star text-warning"></span>
-                      <span class="icon-star text-warning"></span>
-                      <span class="icon-star text-secondary"></span>
-                      <span class="review">(3 Reviews)</span>
-                    </p>
-                  </div>
-              </div>
-            @endforeach
-   
-          <div class="col-lg-6">
- 
-            <div class="d-block d-md-flex listing">
-              <a href="listings-single.html" class="img d-block" style="background-image: url('{{asset('classyads/images/img_1.jpg')}}"></a>
-              <div class="lh-content">
-                <span class="category">Cars &amp; Vehicles</span>
-                <a href="#" class="bookmark"><span class="icon-heart"></span></a>
-                <h3><a href="listings-single.html">Red Luxury Car</a></h3>
-                <address>Don St, Brooklyn, New York</address>
-                <p class="mb-0">
-                  <span class="icon-star text-warning"></span>
-                  <span class="icon-star text-warning"></span>
-                  <span class="icon-star text-warning"></span>
-                  <span class="icon-star text-warning"></span>
-                  <span class="icon-star text-secondary"></span>
-                  <span class="review">(3 Reviews)</span>
-                </p>
-              </div>
-            </div>
+            <div class="col-lg-6">
+@for($i = 0 ; $i <= (count($products)/2) + 1  ; $i++ )
 
-            <div class="d-block d-md-flex listing">
-              <a href="listings-single.html" class="img d-block" style="background-image: url('{{asset('classyads/images/img_2.jpg')}}"></a>
-              <div class="lh-content">
-                <span class="category">Real Estate</span>
-                <a href="#" class="bookmark"><span class="icon-heart"></span></a>
-                <h3><a href="listings-single.html">House with Swimming Pool</a></h3>
-                <address>Don St, Brooklyn, New York</address>
-                <p class="mb-0">
-                  <span class="icon-star text-warning"></span>
-                  <span class="icon-star text-warning"></span>
-                  <span class="icon-star text-warning"></span>
-                  <span class="icon-star text-warning"></span>
-                  <span class="icon-star text-secondary"></span>
-                  <span class="review">(3 Reviews)</span>
-                </p>
-              </div>
-            </div>
-            <div class="d-block d-md-flex listing">
-                <a href="listings-single.html" class="img d-block" style="background-image: url('{{asset('classyads/images/img_3.jpg')}}"></a>
-                <div class="lh-content">
-                  <span class="category">Furniture</span>
-                  <a href="#" class="bookmark"><span class="icon-heart"></span></a>
-                  <h3><a href="listings-single.html">Wooden Chair &amp; Table</a></h3>
-                  <address>Don St, Brooklyn, New York</address>
-                  <p class="mb-0">
-                    <span class="icon-star text-warning"></span>
-                    <span class="icon-star text-warning"></span>
-                    <span class="icon-star text-warning"></span>
-                    <span class="icon-star text-warning"></span>
-                    <span class="icon-star text-secondary"></span>
-                    <span class="review">(3 Reviews)</span>
-                  </p>
+                <div class="d-block d-md-flex listing">
+                    <a href="listings-single.html" class="img d-block" style="background-image: url('{{ asset('storage/'.$products[$i]->image) }}')"></a>
+                    <div class="lh-content">
+                        <span class="category">{{ $products[$i]->category->title ?? "empty" }}</span>
+                        <a href="#" class="bookmark"><span class="icon-heart"></span></a>
+                        <h3><a href="listings-single.html">{{ $products[$i]->title }}</a></h3>
+                        <address> {{ $products[$i]->address }} </address>
+                        <p class="mb-0">
+                            <span class="icon-star text-warning"></span>
+                            <span class="icon-star text-warning"></span>
+                            <span class="icon-star text-warning"></span>
+                            <span class="icon-star text-warning"></span>
+                            <span class="icon-star text-secondary"></span>
+                            <span class="review">({{$products[$i]->reviews}} Reviews)</span>
+                        </p>
+                    </div>
                 </div>
-              </div>
 
-            </div>  
+@endfor
+            </div>
+            <div class="col-lg-6">
+                @for($i = 3 ; $i >= (count($products)/2)   ; $i++ )
+
+                    <div class="d-block d-md-flex listing">
+                        <a href="listings-single.html" class="img d-block" style="background-image: url('{{ asset('storage/'.$products[$i]->image) }}')"></a>
+                        <div class="lh-content">
+                            <span class="category">{{ $products[$i]->category->title ?? "empty" }}</span>
+                            <a href="#" class="bookmark"><span class="icon-heart"></span></a>
+                            <h3><a href="listings-single.html">{{ $products[$i]->title }}</a></h3>
+                            <address> {{ $products[$i]->address }} </address>
+                            <p class="mb-0">
+                                <span class="icon-star text-warning"></span>
+                                <span class="icon-star text-warning"></span>
+                                <span class="icon-star text-warning"></span>
+                                <span class="icon-star text-warning"></span>
+                                <span class="icon-star text-secondary"></span>
+                                <span class="review">({{$products[$i]->reviews}} Reviews)</span>
+                            </p>
+                        </div>
+                    </div>
+
+                @endfor
+            </div>
         </div>
-      </div>
     </div>
-    
+</div>
